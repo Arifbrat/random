1e0707f7ecb0afc12df5f0c5d340a97e6598eec3
Arifbrat <89793610+Arifbrat@users.noreply.github.com>
2022-12-09 14:59:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Arifbrat: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@github.com/Arifbrat/Music-1 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Arifbrat: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@github.com/Arifbrat/gitpython ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
